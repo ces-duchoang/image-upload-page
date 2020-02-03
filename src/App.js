d96cfe65dd53b6a2b12735f2abbf20a20a398d4c
DucHoang <hoangtrongminhduc@hotmail.com>
2020-02-03 18:09:48 +0700

    remove test

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ class App extends React.Component {
     copied: false
   };
   render() {
-    console.log('this is test');
     var ListFile = list.map((e, i) => (
       <div className="file-line" key={i}>
         <span>{e.name}</span>
